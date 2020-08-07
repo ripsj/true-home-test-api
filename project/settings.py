a1e50213f6132c8711c2616729a4c18a83c47bed
José Ripoll <jose.ripoll@dacodes.com.mx>
2020-08-06 22:22:03 -0500

    Debug true para la app

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = getenvar('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*', 'true-home-test-api.herokuapp.com']
